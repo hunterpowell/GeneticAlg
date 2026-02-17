e9f98467d76bab3381a308c8b65fd5f77240f2c2
hunterpowell <161683842+hunterpowell@users.noreply.github.com>
2026-02-17 13:48:05 -0800

    testing threading performance increase

--- main.cpp
+++ main.cpp
@@ -11,11 +11,11 @@ int main() {
     sim->runSim();
     sim->showBots();
 
-    system("python graph.py");
-
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << "Time taken by function: " << duration.count() << " microseconds\n";
-
+    
+    system("python graph.py");
+    
     return 0;
 }
